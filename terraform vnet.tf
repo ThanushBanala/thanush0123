8b310b3f0902eca620b23a91a85500dfe371ba7c
Thanush_Banala <117919360+ThanushBanala@users.noreply.github.com>
2024-09-19 11:15:26 +0530

    terraform vnet.tf

---
+++ terraform vnet.tf
@@ -0,0 +1,6 @@
+#terraform vnet code
+resource "azurerm_network_security_group" "example" {
+  name                = "example-security-group"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+}
